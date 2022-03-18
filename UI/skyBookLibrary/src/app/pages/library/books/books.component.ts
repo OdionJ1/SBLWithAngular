45ea9d385f0090515b9fb21790c0c0749ce9d26a
OdionJ1 <odion.asuelimen@yahoo.com>
2022-03-18 15:03:25 +0000

    Completed book search

--- UI/skyBookLibrary/src/app/pages/library/books/books.component.ts
+++ UI/skyBookLibrary/src/app/pages/library/books/books.component.ts
@@ -9,14 +9,32 @@ import { Book } from "src/app/data/models/book";
 })
 export class BookListComponent implements OnInit {
     public searchType: string = "title"
-    public searchValue: string
     public books: Book[]
     public uploadbookModal: boolean = false
+    public searchedBooks: Book[]
+    private _searchValue: string
+
+    public set searchValue(value: string){
+        this._searchValue = value.toLowerCase().trim()
+
+        if(this.searchType === "title"){
+            this.searchedBooks = this.books.filter(book => book.title.toLowerCase().includes(this.searchValue))
+        } else {
+            this.searchedBooks = this.books.filter(book => book.authors.some(author => author.authorName.toLowerCase().includes(this.searchValue)))
+        }
+    }
+
+    public get searchValue(): string {
+        return this._searchValue
+    }
+    
 
     constructor(private bookService: BookService, private userService: UserService){}
 
     async ngOnInit(){
         this.books = await this.bookService.getBooks()
+
+        this.searchedBooks = this.books
     }
 
     async closeUploadModal(): Promise<void>{
